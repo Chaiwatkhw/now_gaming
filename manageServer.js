320f7a39f6bb590689d37dab83937432360ebfb4
Chaiwatkhw <chaiwat.khw@gmail.com>
2024-12-30 23:17:32 +0700

    Testt

---
+++ manageServer.js
@@ -0,0 +1,11 @@
+//manageServer.js
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+
+
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,'public', 'manage.html'));
+});
+
+module.exports = router;
