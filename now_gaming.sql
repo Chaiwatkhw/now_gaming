4e5f4acdebeff3c7dc28eab9b20c0f5605a8c156
Chaiwatkhw <chaiwat.khw@gmail.com>
2025-02-08 11:06:19 +0700

    a

--- now_gaming.sql
+++ now_gaming.sql
@@ -1,76 +1,233 @@
-CREATE DATABASE now_gaming;
-USE now_gaming;
+CREATE DATABASE  IF NOT EXISTS `now_gaming` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `now_gaming`;
+-- MySQL dump 10.13  Distrib 8.0.40, for Win64 (x86_64)
+--
+-- Host: localhost    Database: now_gaming
+-- ------------------------------------------------------
+-- Server version	9.1.0
 
-create table games (
-	game_id int auto_increment primary key,
-    game_title varchar(255) not null,
-    game_image varchar(255),
-    game_description text,
-    game_deleted tinyint default 0,
-    game_category VARCHAR(50)
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-create table historyprice (
-	history_id int primary key auto_increment,
-    game_id int,
-    game_price decimal(10,2),
-    start_date timestamp default current_timestamp,
+--
+-- Table structure for table `cart`
+--
 
-    foreign key (game_id) references games(game_id)
-    on delete restrict on update cascade
-);
+DROP TABLE IF EXISTS `cart`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cart` (
+  `user_id` int NOT NULL,
+  `game_id` int NOT NULL,
+  `price_id` int DEFAULT NULL,
+  `quantity` int DEFAULT '1',
+  `date_added` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`user_id`,`game_id`),
+  KEY `game_id` (`game_id`),
+  KEY `price_id` (`price_id`),
+  CONSTRAINT `cart_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `cart_ibfk_2` FOREIGN KEY (`game_id`) REFERENCES `games` (`game_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `cart_ibfk_3` FOREIGN KEY (`price_id`) REFERENCES `historyprice` (`history_id`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE users (
-    user_id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    role ENUM('user', 'admin') DEFAULT 'user'
-);
+--
+-- Dumping data for table `cart`
+--
 
-CREATE TABLE keygames (
-    keygame VARCHAR(255) PRIMARY KEY,
-    game_id int,
-    key_used tinyint default 0,
-    FOREIGN KEY (game_id) REFERENCES games(game_id)
-    ON UPDATE RESTRICT 
-);
+LOCK TABLES `cart` WRITE;
+/*!40000 ALTER TABLE `cart` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cart` ENABLE KEYS */;
+UNLOCK TABLES;
 
-CREATE TABLE orders (
-    order_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT,
-    order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(user_id) 
-    ON DELETE CASCADE ON UPDATE CASCADE
-);
+--
+-- Table structure for table `games`
+--
 
-CREATE TABLE orderdetail (
-    order_id INT,
-    game_id INT,
-    price_id INT,
-    keygame VARCHAR(255),
-    PRIMARY KEY (order_id, keygame),
-    FOREIGN KEY (order_id) REFERENCES orders(order_id) 
-    ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (keygame) REFERENCES keygames(keygame) 
-    ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (game_id) REFERENCES games(game_id) 
-    ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (price_id) REFERENCES historyprice(history_id)
-    ON DELETE RESTRICT ON UPDATE CASCADE
-);
+DROP TABLE IF EXISTS `games`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `games` (
+  `game_id` int NOT NULL AUTO_INCREMENT,
+  `game_title` varchar(255) NOT NULL,
+  `game_image` varchar(255) DEFAULT NULL,
+  `game_description` text,
+  `game_deleted` tinyint DEFAULT '0',
+  `game_category` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`game_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE cart (
-    user_id INT,
-    game_id INT,
-    price_id INT,
-    quantity INT DEFAULT 1,
-    date_added TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (user_id, game_id),
-    FOREIGN KEY (user_id) REFERENCES users(user_id) 
-    ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (game_id) REFERENCES games(game_id) 
-    ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (price_id) REFERENCES historyprice(history_id)
-    ON DELETE RESTRICT ON UPDATE CASCADE
-);
+--
+-- Dumping data for table `games`
+--
+
+LOCK TABLES `games` WRITE;
+/*!40000 ALTER TABLE `games` DISABLE KEYS */;
+/*!40000 ALTER TABLE `games` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `historyprice`
+--
+
+DROP TABLE IF EXISTS `historyprice`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `historyprice` (
+  `history_id` int NOT NULL AUTO_INCREMENT,
+  `game_id` int DEFAULT NULL,
+  `game_price` decimal(10,2) DEFAULT NULL,
+  `start_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`history_id`),
+  KEY `game_id` (`game_id`),
+  CONSTRAINT `historyprice_ibfk_1` FOREIGN KEY (`game_id`) REFERENCES `games` (`game_id`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `historyprice`
+--
+
+LOCK TABLES `historyprice` WRITE;
+/*!40000 ALTER TABLE `historyprice` DISABLE KEYS */;
+/*!40000 ALTER TABLE `historyprice` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `keygames`
+--
+
+DROP TABLE IF EXISTS `keygames`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `keygames` (
+  `keygame` varchar(255) NOT NULL,
+  `game_id` int DEFAULT NULL,
+  `key_used` tinyint DEFAULT '0',
+  PRIMARY KEY (`keygame`),
+  KEY `game_id` (`game_id`),
+  CONSTRAINT `keygames_ibfk_1` FOREIGN KEY (`game_id`) REFERENCES `games` (`game_id`) ON UPDATE RESTRICT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `keygames`
+--
+
+LOCK TABLES `keygames` WRITE;
+/*!40000 ALTER TABLE `keygames` DISABLE KEYS */;
+/*!40000 ALTER TABLE `keygames` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `orderdetail`
+--
+
+DROP TABLE IF EXISTS `orderdetail`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `orderdetail` (
+  `order_id` int NOT NULL,
+  `game_id` int DEFAULT NULL,
+  `price_id` int DEFAULT NULL,
+  `keygame` varchar(255) NOT NULL,
+  PRIMARY KEY (`order_id`,`keygame`),
+  KEY `keygame` (`keygame`),
+  KEY `game_id` (`game_id`),
+  KEY `price_id` (`price_id`),
+  CONSTRAINT `orderdetail_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `orderdetail_ibfk_2` FOREIGN KEY (`keygame`) REFERENCES `keygames` (`keygame`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `orderdetail_ibfk_3` FOREIGN KEY (`game_id`) REFERENCES `games` (`game_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `orderdetail_ibfk_4` FOREIGN KEY (`price_id`) REFERENCES `historyprice` (`history_id`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orderdetail`
+--
+
+LOCK TABLES `orderdetail` WRITE;
+/*!40000 ALTER TABLE `orderdetail` DISABLE KEYS */;
+/*!40000 ALTER TABLE `orderdetail` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `orders`
+--
+
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `orders` (
+  `order_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int DEFAULT NULL,
+  `order_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`order_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orders`
+--
+
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` enum('user','admin') DEFAULT 'user',
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'nowgaming','nowgaming2025@gmail.com','$2a$10$YACSV25yjg33D57m6RHdFOocASsH3kXUrcg1YTty.ylmpP5fjn966','admin');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'now_gaming'
+--
+
+--
+-- Dumping routines for database 'now_gaming'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-02-08 11:03:40
